9e7227d5fefb83d60c3f2b14983afd0d81899797
Tobias Braunschober <tobias.braunschober@snowflake.com>
2025-10-31 09:09:57 +0100

    Update connector.py
    change to my test table

--- src/connector.py
+++ src/connector.py
@@ -41,25 +41,17 @@ connector = Blueprint('connector', __name__)
 ## Top 10 customers in date range
 dateformat = '%Y-%m-%d'
 
-@connector.route('/customers/top10')
+@connector.route('/tab/last10')
 def customers_top10():
-    # Validate arguments
-    sdt_str = request.args.get('start_range') or '1995-01-01'
-    edt_str = request.args.get('end_range') or '1995-03-31'
     try:
-        sdt = datetime.datetime.strptime(sdt_str, dateformat)
-        edt = datetime.datetime.strptime(edt_str, dateformat)
+        None
     except:
         abort(400, "Invalid start and/or end dates.")
     sql_string = '''
         SELECT
-            o_custkey
-          , SUM(o_totalprice) AS sum_totalprice
-        FROM snowflake_sample_data.tpch_sf10.orders
-        WHERE o_orderdate >= '{sdt}'
-          AND o_orderdate <= '{edt}'
-        GROUP BY o_custkey
-        ORDER BY sum_totalprice DESC
+            mykey, myvalue
+        FROM apidb.apis.tab
+        mykey DESC
         LIMIT 10
     '''
     sql = sql_string.format(sdt=sdt, edt=edt)
@@ -70,28 +62,23 @@ def customers_top10():
         abort(500, "Error reading from Snowflake. Check the logs for details.")
 
 ## Monthly sales for a clerk in a year
-@connector.route('/clerk/<clerkid>/yearly_sales/<year>')
+@connector.route('/area/<myarea>/key/<mykey>')
 def clerk_montly_sales(clerkid, year):
     # Validate arguments
-    try: 
-        year_int = int(year)
-    except:
-        abort(400, "Invalid year.")
-    if not clerkid.isdigit():
-        abort(400, "Clerk ID can only contain numbers.")
-    clerkid_str = f"Clerk#{clerkid}"
+    if not myarea.isdigit():
+        abort(400, "Area can only contain numbers.")
+    if not mykey.isdigit():
+        abort(400, "Key can only contain numbers.")
+
     sql_string = '''
         SELECT
-            o_clerk
-          ,  Month(o_orderdate) AS month
-          , SUM(o_totalprice) AS sum_totalprice
-        FROM snowflake_sample_data.tpch_sf10.orders
-        WHERE Year(o_orderdate) = {year}
-          AND o_clerk = '{clerkid}'
-        GROUP BY o_clerk, month
-        ORDER BY o_clerk, month
+            mykey, myvalue, myarea
+        FROM apidb.apis.tab
+        WHERE mykey = {mykey}
+          AND myarea = '{myarea}'
+        ORDER BY myarea, mykey
     '''
-    sql = sql_string.format(year=year_int, clerkid=clerkid_str)
+    sql = sql_string.format(mykey=int(mykey), myarea=int(myarea))
     try:
         res = conn.cursor(DictCursor).execute(sql)
         return make_response(jsonify(res.fetchall()))
